d31d9c944b75b27b5a1de57ac8be6f1b72ab600b
Jozo132 <jozo132@gmail.com>
2020-07-06 22:48:06 +0200

    Updated example 1

--- example_1.js
+++ example_1.js
@@ -1,60 +1,84 @@
 // @ts-check
 'use strict'
 
-const GA = require('./ga')
+const GA = require('./vovk-ga')
+const trainer = new GA()
 
+console.log(`Example 1: Simple function optimization`)
 
 const MY_FUNCTION = (x, y, z) => Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2))
 
-const myFitnessFunction = (sample, returnFitness) => {
-    const { x, y, z } = sample
+// (1) Direct return fitness function
+// const myFitnessFunction = (sample) => {
+//     const { x, y, z } = sample
+//     let desired_output = Math.PI               // Desired function output
+//     let actual_output = MY_FUNCTION(x, y, z)   // My function
+//     let loss = Math.abs(desired_output - actual_output)
+//     return loss
+// }
 
-    let desired_output = Math.PI               // Desired function output
-    let actual_output = MY_FUNCTION(x, y, z)   // My function
+// (2) Callback return fitness function
+// const myFitnessFunction = (sample, callback) => {
+//     const { x, y, z } = sample
+//     let desired_output = Math.PI               // Desired function output
+//     let actual_output = MY_FUNCTION(x, y, z)   // My function
+//     let loss = Math.abs(desired_output - actual_output)
+//     callback(loss)
+// }
 
-    let error = Math.abs(desired_output - actual_output)
-    returnFitness(error)
-}
+// (3) Promise return fitness function
+const myFitnessFunction = (sample) => new Promise((resolve, reject) => {
+    try {
+        const { x, y, z } = sample
+        let desired_output = Math.PI               // Desired function output
+        let actual_output = MY_FUNCTION(x, y, z)   // My function
+        let loss = Math.abs(desired_output - actual_output)
+        resolve(loss)
+    } catch (e) { reject(e) }
+})
 
 const parameters = [
     { variable: 'x', type: 'number', range: { min: -20, max: 20 } },
-    { variable: 'y', type: 'number', range: { min: -20, max: 20 } },
-    { variable: 'z', type: 'number', range: { min: -20, max: 20 }, snap: 0.0001 },
+    { variable: 'y', type: 'number', range: { min: -20, max: 20 }, snap: 1 },
+    { variable: 'z', type: 'number', range: { min: -20, max: 20 }, snap: 0.1 },
     // { variable: 'p', type: 'option', options: [1.0, 1.2, 1.4, 1.6, 1.8, 2.0] }
 ]
 
-//const trained_GA_config = { "generations": 100, "population": 1000, "survivor_pct": 0.134, "crossoverChance": 0.1, "mutationChance": 0.332, "mutationPower": 0.0015 } // Fitness:  0.000000000073
-const trained_GA_config = { "generations": 100, "population": 1000, "survivor_pct": 0.05, "crossoverChance": 0.2487, "mutationChance": 0.4853, "mutationPower": 0.24856000000000003 }
 
 
-const trainer = new GA()
 
-trainer.configure({
+const logProgress = progress => {
+    console.log(progress.message)
+}
+
+const logResult = result => {
+    console.log(result.message)
+    const { x, y, z } = result.parameters
+    console.log(`Test ${MY_FUNCTION.toString()}    --->     ${MY_FUNCTION(x, y, z)}`)
+}
+
+const catchError = e => {
+    if (e === 'timeout') console.log('Training timeout!')
+    else throw e
+}
+
+const config = {
     debug: false,
-    maxPopulation: trained_GA_config.population,
+    maxPopulation: 1000,
     //survivors: 10,
-    survivorsPERCENT: trained_GA_config.survivor_pct,
-    crossoverChance: trained_GA_config.crossoverChance,
-    mutationChance: trained_GA_config.mutationChance,
-    mutationPower: trained_GA_config.mutationPower,
+    survivorsPERCENT: 0.05,
+    crossoverChance: 0.25,
+    mutationChance: 0.50,
+    mutationPower: 0.25,
     parameters: parameters,
     initialValues: { x: 1, y: 1, z: 1 },
     fitnessTargetValue: 0,
+    //fitnessTargetTolerance: 1e-15,
     bestSurvive: true,
     fitnessFunction: myFitnessFunction,
-    fitnessTimeout: 10000,
-    fitnessTimeoutFunction: () => console.log('Training timeout!')
-})
+    fitnessTimeout: 10000
+}
 
-
-
-trainer.evolve(100,
-    (status) => { // Progress
-        console.log(status.message)
-    },
-    (finalStatus) => { // Final results
-        console.log(finalStatus.message)
-        const { x, y, z } = finalStatus.parameters
-        console.log(`Test ${MY_FUNCTION.toString()}    --->     ${MY_FUNCTION(x, y, z)}`)
-    }
-)
+trainer.configure(config).evolve(100, logProgress)
+    .then(logResult)
+    .catch(catchError)
